a479c2071eb16180fae478dbe5cdf08368a3079f
Moises Gonzalez <moisesgonzalez2001@hotmail.com>
2015-07-14 10:37:52 -0430

    Se creo funcion para seleccionar turno de los player1 y player2

--- main.cpp
+++ main.cpp
@@ -7,23 +7,26 @@
 
 #include<iostream>
 #include<wchar.h>
+#include<stdlib.h>
+#include<time.h> 
+#include<string>
 
 using namespace std;
 
 void init_matriz();
 void print_matriz();
-void turn_player(char player_name[20], char type);
+void turn_player(char player_name[255], char type);
 void game();
+void select_turn();
 
 char matriz[3][3];
-char player1[255] = "Player1";
-char player2[255] = "Player2";
+int hora = time(NULL);
+char player1[255];
+char player2[255];
 
 int main(){
-  
     init_matriz();
     game();
-    
     return 0;
 }
 
@@ -47,7 +50,7 @@ void print_matriz(){
     }
 }
 
-void turn_player(char player_name[255],char type){
+void turn_player(char player_name[255], char type){
     int x,y;
     cout<<endl<<"Turno de: "<<player_name<<endl;
     print_matriz();
@@ -62,6 +65,64 @@ void turn_player(char player_name[255],char type){
 }
 
 void game(){
+    select_turn();
     turn_player(player1,'X'); 
     turn_player(player2,'O');
+}
+
+void select_turn(){
+    int turn, numRand;
+    char option;
+    bool sw;
+    
+    do{
+        cout<<"Cara o Sello?"<<endl;
+        cout<<"Escriba 'C' si es cara | 'S' si es sello"<<endl;
+        cout<<"Seleccion: ";
+        cin>>option;
+        fflush(stdin);
+        if(option == 'C' || option == 'c'){
+            turn = 0;
+            sw = true;
+        }
+        else if(option == 'S' || option == 's'){
+            turn = 1;
+            sw = true;
+        }
+        else{
+            cout<<"Seleccion incorrecta..."<<endl;
+            sw = false;
+            getwchar();
+        }
+    }while(!sw);
+    
+    srand(hora);
+    numRand = rand() % 11;
+    numRand = numRand % 2;
+    if( numRand == 0){
+        cout<<"Salio Cara"<<endl;
+        if(turn == 0){
+            cout<<"Felicidades comienzas primero!"<<endl;
+            strcpy(player1,"Player");
+            strcpy(player2,"CPU");
+        }
+        else{
+            cout<<"Comienzas segundo"<<endl;
+            strcpy(player1,"CPU");
+            strcpy(player2,"Player");
+        }
+    }
+    else if(numRand == 1){
+        cout<<"Salio Sello"<<endl;
+        if(turn == 1){
+            cout<<"Felicidades comienzas primero!"<<endl;
+            strcpy(player1,"Player");
+            strcpy(player2,"CPU");
+        }
+        else{
+            cout<<"Comienzas segundo"<<endl;
+            strcpy(player1,"CPU");
+            strcpy(player2,"Player");
+        }
+    }
 }
